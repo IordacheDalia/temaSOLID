8c924fbc377dd2fcea7cacaf6102f9690e2fca48
IordacheDalia <dalia.iordache@gmail.com>
2023-03-08 22:22:21 +0200

    second commit

--- src/ro/ase/acs/cts/main/Main.java
+++ src/ro/ase/acs/cts/main/Main.java
@@ -5,7 +5,7 @@ import ro.ase.acs.cts.classes.*;
 public class Main {
     public static void main(String[] args) {
         Manager manager=new Manager(new DatabaseConnectionOperation(), new CreateTableOperation(), new InsertDataOperation(),new ReadDataOperation(),new WriteDataOperation());
-        //manager.run();
+        manager.run();
 
 
     }
